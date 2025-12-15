42ee3dcb7c245caf99dda9886c0918edc03f8ab9
MarioGrateronDuoc <ma.grateron@duocuc.cl>
2025-12-15 14:03:55 -0300

    Configuracion de error

--- User/src/main/java/com/example/User/security/SecurityConfig.java
+++ User/src/main/java/com/example/User/security/SecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.config.Customizer; 
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -28,6 +29,7 @@ public class SecurityConfig {
                 new JwtAuthenticationFilter(jwtUtil);
 
         http
+            .cors(Customizer.withDefaults())
             .csrf(csrf -> csrf.disable())
             .sessionManagement(session ->
                 session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
