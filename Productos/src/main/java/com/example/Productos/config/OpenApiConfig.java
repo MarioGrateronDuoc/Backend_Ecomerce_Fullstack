fa4e316e36cf2d7eae2d13978867071fd6736393
MarioGrateronDuoc <ma.grateron@duocuc.cl>
2025-12-15 12:59:59 -0300

    Swagger

--- Productos/src/main/java/com/example/Productos/config/OpenApiConfig.java
+++ Productos/src/main/java/com/example/Productos/config/OpenApiConfig.java
@@ -2,6 +2,9 @@ package com.example.Productos.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.Components;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -10,10 +13,25 @@ public class OpenApiConfig {
 
     @Bean
     public OpenAPI customOpenAPI() {
+
+        final String securitySchemeName = "bearerAuth";
+
         return new OpenAPI()
-            .info(new Info()
-                .title("API de Productos")
-                .version("1.0.0")
-                .description("Microservicio para gestionar la información de productos (CRUD)."));
+                .info(new Info()
+                        .title("API de Productos")
+                        .version("1.0.0")
+                        .description("Microservicio para gestionar la información de productos (CRUD)."))
+                // Seguridad global
+                .addSecurityItem(new SecurityRequirement().addList(securitySchemeName))
+                // Esquema JWT
+                .components(new Components()
+                        .addSecuritySchemes(securitySchemeName,
+                                new SecurityScheme()
+                                        .name(securitySchemeName)
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")
+                        )
+                );
     }
 }
--- User/src/main/java/com/example/User/Config/OpenApiConfig.java
+++ User/src/main/java/com/example/User/Config/OpenApiConfig.java
@@ -2,6 +2,9 @@ package com.example.User.Config;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.Components;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -10,10 +13,25 @@ public class OpenApiConfig {
 
     @Bean
     public OpenAPI customOpenAPI() {
+
+        final String securitySchemeName = "bearerAuth";
+
         return new OpenAPI()
-            .info(new Info()
-                .title("API de Productos")
-                .version("1.0.0")
-                .description("Microservicio para gestionar la información de productos (CRUD)."));
+                .info(new Info()
+                        .title("API de Usuarios")
+                        .version("1.0.0")
+                        .description("Microservicio para gestionar la información de los Usuarios (CRUD)."))
+                //  Esto agrega el candado global
+                .addSecurityItem(new SecurityRequirement().addList(securitySchemeName))
+                //  Esto define el esquema JWT
+                .components(new Components()
+                        .addSecuritySchemes(securitySchemeName,
+                                new SecurityScheme()
+                                        .name(securitySchemeName)
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")
+                        )
+                );
     }
 }
